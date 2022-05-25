4a15f9ade0c43e2dd3725bca9912eb9c967ca1e5
Darthsae <70599537+Darthsae@users.noreply.github.com>
2022-05-25 11:10:31 -0400

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -14,16 +14,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(ArsMortem.MODID)
+public class ArsMortem
 {
     // Directly reference a log4j logger.
     public static ForgeConfigSpec SERVER_CONFIG;
-    public static final String MODID = "examplemod";
+    public static final String MODID = "arsmortem";
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public ArsMortem() {
         ArsNouveauRegistry.registerGlyphs();
         ExampleConfig.registerGlyphConfigs();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
